88d1624edf327b7ed868c7bbe63b1078436f8f77
gal-samuel-israel <webmaster@agency.co.il>
2022-08-22 15:07:47 +0300

    Add gamification block

---
+++ javascripts/discourse/components/gamification.js
@@ -0,0 +1,24 @@
+import Component from "@glimmer/component";
+import { ajax } from "discourse/lib/ajax";
+import { tracked } from "@glimmer/tracking";
+
+export default class Gamification extends Component {
+  @tracked gamificationList = null;
+
+  constructor() {
+    super(...arguments);
+
+    const count = this.args?.params?.count || 5;
+
+    ajax(`/leaderboard`)
+    .then((scores) => {
+        this.gamificationList = scores;
+        console.log(scores);
+      }
+    );
+  }
+
+  willDestroy() {
+    this.gamificationList = null;
+  }
+}
