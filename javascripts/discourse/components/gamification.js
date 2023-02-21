c2b34527b3485f557cc0c97ab3d444611c34dd2b
gal-samuel-israel <webmaster@agency.co.il>
2023-02-21 18:38:00 +0200

    add params

--- javascripts/discourse/components/gamification.js
+++ javascripts/discourse/components/gamification.js
@@ -52,9 +52,8 @@ export default class Gamification extends Component {
       ajax(`/admin/plugins/explorer/queries/11/run`,{
         type: "POST",
         headers: { "Api-Username": "system", "Api-Key": "d0082b555db3459e85fee2d29b29b79edc689d8767a80fef33761ef16869d83c" }, //Data Explorer Ready Only - query 11
-        data: {
-          user_id: 2
-        }})
+        data: {"params": { "user_id": 2 }}
+      })
       .then((response) => {        
         console.log(response);
       }).finally(() => this.loading = false)
