9069aae93905abb47b5c79ff63b2e5b0b4071259
gal-samuel-israel <webmaster@agency.co.il>
2023-02-23 15:37:14 +0200

    add computed ember object

--- javascripts/discourse/components/algo-badge.js
+++ javascripts/discourse/components/algo-badge.js
@@ -1,15 +1,20 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { empty, equal, notEmpty } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
 
 export default class AlgoBadge extends Component {
   @service site;
+  
+  @notEmpty("args.userId")
+  userId;
+
   @tracked algoBadge = null;
 
   constructor() {
     super(...arguments);
     this.algoBadge = true;
-    console.log(this.args);
+    console.log(this.args, userId);
   }
 
   willDestroy() {
