311392ee7e6a5c5401be9aad0732e5517c44a193
gal-samuel-israel <webmaster@agency.co.il>
2023-02-23 18:05:30 +0200

    no split

--- javascripts/discourse/components/algo-badge.js
+++ javascripts/discourse/components/algo-badge.js
@@ -51,7 +51,7 @@ export default class AlgoBadge extends Component {
           */
           this.algoBadge = true;
           this.algoBadgeInfo = JSON.parse(obj.badges);
-          console.log(this.algoBadgeInfo.split(','));
+          console.log(this.algoBadgeInfo);
           this.algoBadgeUrl = JSON.parse(obj.urls);
           console.log(this.algoBadgeUrl[0]);
         } 
