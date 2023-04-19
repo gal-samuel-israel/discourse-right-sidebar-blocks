436ac460957b1d7429656f12e55e80b2e7e988f9
gal-samuel-israel <webmaster@agency.co.il>
2023-04-19 18:51:08 +0300

    add actions for board 2

--- javascripts/discourse/components/gamification.js
+++ javascripts/discourse/components/gamification.js
@@ -19,11 +19,18 @@ export default class Gamification extends Component {
   @tracked page = 1;
   @tracked loading = false;
   @tracked canLoadMore = true;
-  @tracked gamificatinObj = null;
-  @tracked gamificatinObj2 = null;
+  @tracked gamificatinObj = null;  
   @tracked period = "monthly";
   @tracked activeMonthly = true;
   @tracked activeAll = false;
+
+  //for algosec users show the regular users board too
+  @tracked gamificatinObj_2 = null;
+  @tracked period_2 = "monthly";
+  @tracked activeMonthly_2 = true;
+  @tracked activeAll_2 = false;
+  @tracked loading_2 = false;
+  @tracked canLoadMore_2 = true;
 
   debug = false;
   showOnlyToAdmins = false;
@@ -56,7 +63,7 @@ export default class Gamification extends Component {
     if(this.debug){ console.log('isAlgoSecUser:', this.isAlgoSecUser); }
 
     var leaderboardURL = (this.isAlgoSecUser) ? `/leaderboard/4?period=${this.period}`:`/leaderboard/?period=${this.period}`;
-    var secondboardURL = (this.isAlgoSecUser) ? `/leaderboard/?period=${this.period}` : false;
+    var secondboardURL = (this.isAlgoSecUser) ? `/leaderboard/?period=${this.period_2}` : false;
 
     ajax(leaderboardURL)
     .then((scores) => {
@@ -69,8 +76,8 @@ export default class Gamification extends Component {
     if(this.debug && secondboardURL !== false){
       ajax(secondboardURL)
       .then((scores) => {
-          this.gamificatinObj2 = scores;
-          this.gamificatinObj2.users = scores.users.slice(0, this.maxUsersToShow);
+          this.gamificatinObj_2 = scores;
+          this.gamificatinObj_2.users = scores.users.slice(0, this.maxUsersToShow);
           //console.log(scores);
         }
       );  
@@ -98,6 +105,27 @@ export default class Gamification extends Component {
       .catch(popupAjaxError);
   }
 
+  _changePeriod_2(period) {    
+    this.period_2 = period;
+    if(this.debug){ console.log('changePeriod:' + period);  }
+
+    return ajax(
+      `/leaderboard/${this.gamificatinObj_2.leaderboard.id}?period=${this.period_2}`
+    )
+      .then((result) => {
+        if(this.debug){ console.log(result);  }
+        if (result.users.length === 0) {
+          this.canLoadMore_2 = false;
+        }
+        this.page_2 = 1;
+        this.gamificatinObj_2 = result;
+        this.gamificatinObj_2.users = result.users.slice(0, this.maxUsersToShow);
+      })
+      .finally(() => this.loading_2 = false)
+      .catch(popupAjaxError);
+  }
+
+
   @action
   scoreForMonth(event){
     event?.preventDefault();
@@ -111,6 +139,21 @@ export default class Gamification extends Component {
     this.activeMonthly = false; this.activeAll = true;
     this._changePeriod('all');    
   }
+
+  @action
+  scoreForMonth_2(event){
+    event?.preventDefault();
+    this.activeMonthly_2 = true; this.activeAll_2 = false;  
+    this._changePeriod_2('monthly');    
+  }
+
+  @action
+  scoreForAll_2(event){
+    event?.preventDefault();
+    this.activeMonthly_2 = false; this.activeAll_2 = true;
+    this._changePeriod_2('all');    
+  }
+
 
   willDestroy() {
     this.gamificatinObj = null;
