fc7dec6732392a0a4ab808117d58cdfd573ecbc8
gal-samuel-israel <webmaster@agency.co.il>
2023-10-22 12:51:03 +0300

    use registerRawHelper

--- javascripts/discourse/components/gamification.js
+++ javascripts/discourse/components/gamification.js
@@ -4,12 +4,12 @@ import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 //import { registerUnbound } from "discourse-common/lib/helpers"; DEPRECATED 
-//import { registerRawHelper } from "discourse-common/lib/raw-helpers";
-/*
+import { registerRawHelper } from "discourse-common/lib/raw-helpers";
+
 registerRawHelper("helperIncrement", function(value){
   return parseInt(value) + 1;
 });
-*/
+
 const  checkIfGroupIsInUserGroups = function (group, arrGroups) {
   return arrGroups.some(function(el) {
     return el.name.toLowerCase() === group;
