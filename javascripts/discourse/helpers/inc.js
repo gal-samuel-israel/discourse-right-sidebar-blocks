f9ffa7be89afe2eff447f5de5c9bdd88fbc5ed09
Gal Samuel <webmaster@agency.co.il>
2025-04-24 11:55:52 +0300

    250424 - try registerHelper instead of registerRawHelper

--- javascripts/discourse/helpers/inc.js
+++ javascripts/discourse/helpers/inc.js
@@ -1,3 +1,5 @@
+/*
+DEPRECATED on April 2025
 import { registerRawHelper } from "discourse-common/lib/helpers";
 
 registerRawHelper("inc", incHelperFunction);
@@ -5,3 +7,22 @@ registerRawHelper("inc", incHelperFunction);
 export default function incHelperFunction(value) {
     return parseInt(value) + 1;
 }
+*/
+import { registerHelper } from "@ember/template/helpers";
+//import { htmlSafe } from "@ember/template";
+
+function incHelperFunction(value/*, options*/) {
+  const result = parseInt(value, 10) + 1;
+  return result; // Simple number case
+  // return htmlSafe(result.toString()); // Only if you need HTML output
+}
+
+// Export both the function and initializer
+export { incHelperFunction };
+
+export default {
+  name: "inc-helper",
+  initialize(container) {
+    registerHelper("inc", incHelperFunction);
+  }
+};
