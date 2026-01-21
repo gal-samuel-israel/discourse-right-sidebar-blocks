46a1b85f09fcc76de8787af4d0f1107dd8855ce7
gal-samuel-israel <webmaster@agency.co.il>
2026-01-21 17:17:32 +0200

    fix

--- javascripts/discourse/components/custom-html.js
+++ javascripts/discourse/components/custom-html.js
@@ -1,9 +1,12 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 
 export default class CustomHtml extends Component {
+    @tracked content = null;
     debugForAdmins = false;
     constructor() {
         super(...arguments);
+        this.content = this.args?.params?.content;
         this.debugForAdmins = settings?.enable_debug_for_admins; //from settings.yml
         if (this.debugForAdmins) {
             console.log('custom-html constructor called');
@@ -13,7 +16,7 @@ export default class CustomHtml extends Component {
         }
     }
 
-    get content() {
-        return this.args?.params?.content;
+    willDestroy() {
+        this.content = null;
     }
 }
